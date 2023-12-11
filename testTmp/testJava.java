243c82068ce939eff1668eee15ee9235b90fd865
AndreK <andre_15-58@mail.ru>
2023-12-11 23:27:16 +0300

    Test Project Java

--- testTmp/testJava.java
+++ testTmp/testJava.java
@@ -3,5 +3,6 @@ package testTmp;
 public class program {
     static public void main(String[] args) {
         System.out.println("Hellow JAVA!");
+        System.out.println("Test Git Project JAVA!");
     }
 }
